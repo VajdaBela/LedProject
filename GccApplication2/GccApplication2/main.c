695535bde88c2e02c7a13d7f9766f351c8416361
VajdaBela <vajdabela01@gmail.com>
2016-08-04 22:23:45 +0200

    everything comented

--- GccApplication2/GccApplication2/main.c
+++ GccApplication2/GccApplication2/main.c
@@ -12,7 +12,7 @@
 #include <util/delay.h>
 #include <string.h>
 
-
+//logical
 #define A 0
 #define B 1
 #define C 2
@@ -22,32 +22,38 @@
 #define SERIN 5
 #define SRCK 5
 
-
+//number of movements
 volatile uint8_t shiftNum = 0;
+//this will turn on A B C and D
 volatile uint8_t const allRows[8] = {0x07, 0x06, 0x05, 0x04, 0x03, 0x02, 0x01, 0xF0}; // last one for clearing previous
+//no line turned on from the previous
 #define ERASE 7
+//character we are using
 volatile uint8_t const numbers[][7] = { 
 	{0b00001111, 0b00001001, 0b00001001, 0b00001001, 0b00001001, 0b00001001, 0b00001111 },//0
 	{0b00000001, 0b00000011, 0b00000101, 0b00001001, 0b00000001, 0b00000001, 0b00000001 },//1
 	{0b00001111, 0b00000001, 0b00000001, 0b00001111, 0b00001000, 0b00001000, 0b00001111 },//2
 };
+//what should be put in the register
 volatile uint16_t bufer[7] = {0};
 
+//what will be written out
 char text[] = "012";
 
-
+//puts either 1 or 0 into the register 
 void Shift(uint8_t state)
 {
 	if(state)
 		PORTB |= (1 << SERIN);
 	else
 		PORTB &= ~(1 << SERIN);
-		
+	
+	//the actual shift	
 	PORTC |= (1 << SRCK);
 	PORTC &= ~(1 << SRCK);
 }
 
-
+//clears the registers (shift 0, 16 times
 void clear()
 {
 	for(uint8_t br = 0; br < 16; ++br)
@@ -56,7 +62,7 @@ void clear()
 	}
 }
 
-
+//buffer neads to be made (it'll consist only from what is on screen)
 void SetBufer()
 {
 	//clear bufer
@@ -66,10 +72,13 @@ void SetBufer()
 	}
 	
 	uint16_t temp[7] = {0};
+	//go through all of the letters
 	for(uint8_t br = 0; br < strlen(text); ++br)
 	{
+		//if shiftNum - br * 5 - 5 is bigger than 16 the letters left the screen
 		if ((shiftNum - br * 5 - 5) < 16)
 		{
+			//if shiftNum - br * 5 is smaller than 0 the letters didnt yet come to screen(we can discard them, brake is for that)
 			if ((shiftNum - br * 5) < 0)
 			{
 				break;
@@ -84,27 +93,31 @@ void SetBufer()
 			//set bufer
 			for (uint8_t br1 = 0; br1 < 7; ++br1)
 			{
-				if((shiftNum - 4 - br * 5) > 0)
+				//because  shifting with a negative is not defined in c there are two cases
+				if((shiftNum - 4 - br * 5) > 0)//for positiv
 				{
 					bufer[br1] |= (temp[br1] << (shiftNum - 4 - br * 5));
 				}
-				else
+				else // and negative
 					bufer[br1] |= (temp[br1] >> (shiftNum - 4 - br * 5) * (-1));
-				
+				/*the letters in a bufer will be mooved either left or right(right if it's beig cut off) this mutch: shiftNum - 4 - br * 5 
+				/shift num is logical, 4 is so that we dont start with the first letter already on screen, br * 5 is the letter in the row times 5
+				/ 5 is used because a letter consists of 4 diods + 'space'
+				 */
 			}
 		}
 	}
 }
 
 
-
+//firs turns off all existing rows then turns on one
 void TurnOnRow(uint8_t row)
 {
 	PORTB &= allRows[ERASE];
 	PORTB |= allRows[row];
 }
 
-
+//sets up timer with a prescaler of 1024 (currently counts to 500 ms)
 void setupTimer()
 {
 	TIMSK1 |= (1 << OCIE1A);
@@ -113,7 +126,7 @@ void setupTimer()
 	sei();
 }
 
-
+//reads data and puts into the register
 void PutData(uint16_t data)
 {
 	//acording to Vajda
@@ -171,7 +184,7 @@ int main()
 	}
 }
 
-
+//interupts evry 500 ms and moovs the letters by 1 (goes until (5 * strlen(text) ) + 16 ) and then starts all over )
 ISR(TIMER1_COMPA_vect)
 {
 	shiftNum++;
@@ -180,3 +193,4 @@ ISR(TIMER1_COMPA_vect)
 		shiftNum = 0;
 	}
 }
+//extra new line so that compiler is happy :^)
